49c78a258a053c6ab5bdc9df91a21b6503f0a440
globewalker91 <47368660+globewalker91@users.noreply.github.com>
2025-08-29 22:54:25 -0500

    Update api_client.py

--- utils/api_client.py
+++ utils/api_client.py
@@ -1,1 +1,22 @@
+import requests
+
+class BookingAPI:
+    BASE_URL = "https://restful-booker.herokuapp.com"
+
+    def create_booking(self, firstname, lastname):
+        url = f"{self.BASE_URL}/booking"
+        payload = {
+            "firstname": firstname,
+            "lastname": lastname,
+            "totalprice": 120,
+            "depositpaid": True,
+            "bookingdates": {
+                "checkin": "2025-08-20",
+                "checkout": "2025-08-25"
+            },
+            "additionalneeds": "Breakfast"
+        }
+        resp = requests.post(url, json=payload)
+        resp.raise_for_status()
+        return resp.json()
 

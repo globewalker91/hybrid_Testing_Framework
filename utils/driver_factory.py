57fdfaa6ccd1b5ad655d8424d06f3d084e104b2b
globewalker91 <47368660+globewalker91@users.noreply.github.com>
2025-08-29 22:54:56 -0500

    Update driver_factory.py

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -1,1 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def get_driver():
+    options = Options()
+    options.add_argument("--headless")  # comment this line to see browser
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Chrome(options=options)
+    driver.implicitly_wait(10)
+    return driver
 

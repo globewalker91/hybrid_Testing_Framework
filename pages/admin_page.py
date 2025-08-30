21c821e84ff5933fbf61ef0f05dfc18d1c02652e
globewalker91 <47368660+globewalker91@users.noreply.github.com>
2025-08-29 22:55:25 -0500

    Update admin_page.py

--- pages/admin_page.py
+++ pages/admin_page.py
@@ -1,1 +1,19 @@
+from selenium.webdriver.common.by import By
+
+class AdminPage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def open(self, url):
+        self.driver.get(url)
+
+    def login(self):
+        # Default creds for demo site
+        self.driver.find_element(By.ID, "username").send_keys("admin")
+        self.driver.find_element(By.ID, "password").send_keys("password")
+        self.driver.find_element(By.ID, "doLogin").click()
+
+    def is_booking_present(self, firstname, lastname):
+        bookings = self.driver.find_elements(By.CSS_SELECTOR, ".row .col-sm-3 p")
+        return any(firstname in b.text or lastname in b.text for b in bookings)
 
